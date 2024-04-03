27ba372e18a221a272cbeafa827d21b0f8c82379
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-04-04 00:31:32 +0600

    navbar added with dynamic route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,9 @@ import {
     createBrowserRouter
 } from "react-router-dom";
 import App from "../App";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import Career from "../Pages/Career/Career";
 
 
 const router = createBrowserRouter([
@@ -11,8 +14,16 @@ const router = createBrowserRouter([
         children: [
             {
                 path:'/',
-                element
-            }
+                element : <Home />
+            },
+            {
+                path:'/about',
+                element : <About />
+            },
+            {
+                path:'/career',
+                element : <Career />
+            },
         ]
     },
 ]);
